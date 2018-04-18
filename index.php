082a009fe8f31db79839ccf22e8ddd9911922bc5
justinb19 <38440942+justinb19@users.noreply.github.com>
2018-04-18 16:28:34 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Test</title>
+<link rel="stylesheet" href="./css/style.css" />
+</head>
+<body>
+ 				<p style="color: red; font-size: 12px">Testtest</p>
+				 <h1>ü1	jfnfnf</h>
+				<h2>hdhrjjfjf</h2>
+</body>
+</html>
